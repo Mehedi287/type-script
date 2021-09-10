cf308b73ecd4d1bc951941bb0a3e4c00f0b6a63f
Mehedi390 <mh0816150@gmail.com>
2021-09-09 19:48:53 -0700

    useing arrow function in ts

---
+++ arrowFunction.js
@@ -0,0 +1,6 @@
+var sum = function (numner1, num2) {
+    var result = num2 + numner1;
+    console.log(result);
+    return result;
+};
+sum(45, 57);
---
+++ arrowFunction.ts
@@ -0,0 +1,8 @@
+const sum = (numner1: number, num2: number): number => {
+    const result = num2 + numner1;
+    console.log(result);
+
+    return result;
+
+}
+sum(45, 57)
