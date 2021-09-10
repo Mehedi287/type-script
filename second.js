bddcf3b052245e44719a75f57e8724e615c50ca6
Mehedi390 <mh0816150@gmail.com>
2021-09-09 18:39:53 -0700

    type variable declair

---
+++ second.js
@@ -0,0 +1,10 @@
+function add(fastNumber, SecondNumber) {
+    var result = fastNumber + SecondNumber;
+    console.log(result);
+    return result;
+}
+add(454, 876);
+var a = '45';
+var b = 9;
+var c = a + b;
+console.log(c);
---
+++ second.ts
@@ -0,0 +1,13 @@
+function add(fastNumber: number, SecondNumber: number) {
+    const result: number = fastNumber + SecondNumber;
+    console.log(result);
+    return result;
+}
+add(454, 876)
+const a: string = '45';
+const b: number = 9;
+const c = a + b;
+console.log(c);
+
+
+
