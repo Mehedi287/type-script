6fc3f4e5da8833a959f5f487ec4075aa48030766
Mehedi390 <mh0816150@gmail.com>
2021-09-09 19:34:32 -0700

    objacr using in ts

---
+++ objact.js
@@ -0,0 +1,13 @@
+var student = {
+    name: 'hasan',
+    age: 452,
+    salary: 9808,
+    id: 65
+};
+var sakib = {
+    name: 'sakib',
+    age: 4456,
+    salary: 65635,
+    id: 45423
+};
+console.log(sakib);
---
+++ objact.ts
@@ -0,0 +1,19 @@
+type persone = {
+    name: string,
+    age: number,
+    salary: number,
+    id: number
+}
+const student: persone = {
+    name: 'hasan',
+    age: 452,
+    salary: 9808,
+    id: 65
+}
+const sakib: persone = {
+    name: 'sakib',
+    age: 4456,
+    salary: 65635,
+    id: 45423
+}
+console.log(sakib);
